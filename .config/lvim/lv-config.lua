62248184408037d3b48b4ddf75f0017be2b1f020
Tore Pettersen <toreskog@live.com>
2021-08-09 22:41:37 +0200

    Add LunarVim config

---
+++ .config/lvim/lv-config.lua
@@ -0,0 +1,56 @@
+
+-- general
+vim.opt.relativenumber = true
+vim.g.nvim_tree_disable_netrw = false
+
+lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "onedark"
+
+-- keymappings
+lvim.leader = "space"
+
+-- Save
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-s>", "<ESC>:w<CR>", { noremap = true, silent = true })
+
+-- Buffers
+vim.api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<TAB-S>", ":BufferPrevious<CR>", { noremap = true, silent = true })
+
+-- Which key
+lvim.builtin.which_key.mappings["f"] = {"<cmd>Files<CR>", "Find file"}
+lvim.builtin.which_key.mappings["l"] = {"<cmd>Rg<CR>", "Live grep"}
+lvim.builtin.which_key.mappings["b"] = {
+  name = "+Buffers",
+  t = {"<cmd>BufferPin<CR>", "Pin buffer"},
+  e = {"<cmd>BufferCloseAllButCurrent<CR>", "Close all, but current buffer"},
+  r = {"<cmd>BufferCloseAllButPinned<CR>", "Close all, but pinned buffers"},
+  j = {"<cmd>BufferPick<CR>", "Jump to buffer"},
+  c = {"<cmd>BufferClose<CR>", "Close buffer"},
+  D = {"<cmd>BufferOrderByDirectory<CR>", "Order by directory"},
+}
+
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.galaxyline.active = true
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.compe.autocomplete = true
+lvim.builtin.compe.documentation = true
+
+-- Elixir
+lvim.lang.elixir.formatters = {
+  {
+    exe = "mix",
+    args = {"fmt"}, 
+  },
+}
+
+-- Additional Plugins
+lvim.plugins = {
+  {"joshdick/onedark.vim"},
+  {"junegunn/fzf", run = "./install --all"},
+  {"junegunn/fzf.vim"},
+}
+
